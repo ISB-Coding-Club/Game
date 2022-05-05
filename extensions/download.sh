525b176e19a66c94a0f1d5b01dcb07202e053d62
Jacob Sapoznikow <jacob1coder@gmail.com>
2022-05-05 18:58:53 +0000

    Remove stuff that doesn't work

--- extensions/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-wget -qO vsliveshare.vsix --show-progress "$(cat vsliveshare.vsix.meta)"
-
-wget -qO vsliveshare-audio.vsix --show-progress "$(cat vsliveshare-audio.vsix.meta)"
-
-wget -qO vsliveshare-pack.vsix --show-progress "$(cat vsliveshare-pack.vsix.meta)"
